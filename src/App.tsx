b8533a0158316052a18df2d26c61dbfecec3584e
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-14 15:11:17 -0700

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function App() {
-  return <div className='App'>test</div>
+  return (
+    <div className='App'>
+      <button className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
+        Helllo
+      </button>
+    </div>
+  )
 }
 
 export default App
