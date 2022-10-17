3473b24b32d35b65ffab3018af7b74925b9a4554
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-17 14:54:05 -0700

    Both pages on app.js

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import NavDropdown from './NavDropdown'
-import Logo from './Logo'
 
 type AppProps = {
   setPage: (page: string) => void
@@ -10,10 +9,7 @@ const Navbar = ({ setPage }: AppProps) => {
   return (
     <header className='mb-2 px-4 shadow'>
       <div className='relative mx-auto flex max-w-screen-lg flex-col py-4 sm:flex-row sm:items-center sm:justify-between'>
-        <a className='flex items-center text-2xl font-black' href='/'>
-          <span className='mr-2 text-3xl text-red-600'>
-            <Logo />
-          </span>
+        <a className='flex items-center text-3xl font-black' href='/'>
           <span>Schwifty Facts</span>
         </a>
         <input className='peer hidden' type='checkbox' id='navbar-open' />
@@ -29,12 +25,12 @@ const Navbar = ({ setPage }: AppProps) => {
         >
           <ul className='flex flex-col gap-y-4 sm:flex-row sm:gap-x-8'>
             <li onClick={() => setPage('people')}>
-              <a className='text-gray-600 hover:text-blue-600' href='#'>
+              <a className='text-teal hover:text-blue-600' href='#'>
                 Characters
               </a>
             </li>
             <li onClick={() => setPage('planets')}>
-              <a className='text-gray-600 hover:text-blue-600' href='#'>
+              <a className='text-teal hover:text-blue-600' href='#'>
                 Planets
               </a>
             </li>
