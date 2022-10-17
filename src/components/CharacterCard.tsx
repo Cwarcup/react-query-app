7d359761065d5202ec8a1ce5820fffee281353d7
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-17 14:23:56 -0700

    characters page

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -4,14 +4,12 @@ import { Character } from '../types/RickAndMortyTypes'
 const CharacterCard = (character: Character) => {
   console.log(character)
   return (
-    <div className='mx-auto my-10 flex max-w-md flex-col rounded-2xl bg-white px-4 shadow md:max-w-5xl md:flex-row md:items-center'>
+    <div className='mx-auto my-10 flex max-w-md flex-col rounded-2xl bg-sand px-4 shadow md:max-w-2xl md:flex-row md:items-center'>
       <div className='shrink-0 my-4 md:mr-8 md:max-w-sm'>
-        <img className='rounded-2xl' src={character.image} alt='' />
+        <img className='rounded-2xl drop-shadow-md' src={character.image} alt='character image' />
       </div>
       <div className='py- sm:py-8'>
-        <a href='#' className='mb-6 block text-2xl font-medium text-gray-700'>
-          {character.name}
-        </a>
+        <p className='mb-6 block text-4xl font-medium text-gray-700'>{character.name}</p>
         <p className='mb-6 text-gray-500'>
           {character.status} - {character.species}
         </p>
--- src/components/CharactersPage.tsx
+++ src/components/CharactersPage.tsx
@@ -4,6 +4,7 @@ import { PacmanLoader } from 'react-spinners'
 import { FetchData } from '../fetchers/FetchData'
 import CharacterCard from './CharacterCard'
 import { Character } from '../types/RickAndMortyTypes'
+import Header from '../TailwindComponents/HeaderText'
 
 const CharactersPage = () => {
   const [page, setPage] = React.useState(1)
@@ -41,17 +42,24 @@ const CharactersPage = () => {
   // return if successful
   return (
     <>
-      <h2>Characters</h2>
-      <button onClick={() => setPage((old) => Math.max(old - 1, 1))} disabled={page === 1}>
-        Previous Page
-      </button>
-      <span>{page}</span>
-      <button
-        onClick={() => setPage((old) => (!data || !data.info.next ? old : old + 1))}
-        disabled={!data || !data.info.next}
-      >
-        Next Page
-      </button>
+      <Header text='Characters' />
+      <div className='flex w-80 justify-between mt-5'>
+        <button
+          className='bg-sand hover:bg-lightSand text-darkBlue font-bold py-2 px-4 rounded'
+          onClick={() => setPage((old) => Math.max(old - 1, 1))}
+          disabled={page === 1}
+        >
+          Previous Page
+        </button>
+        <span>{page}</span>
+        <button
+          className='bg-sand hover:bg-lightSand text-darkBlue font-bold py-2 px-4 rounded'
+          onClick={() => setPage((old) => (!data || !data.info.next ? old : old + 1))}
+          disabled={!data || !data.info.next}
+        >
+          Next Page
+        </button>
+      </div>
       <div>
         {data.results.map((character: Character, index: number) => (
           <CharacterCard key={`${character.name}-${index}`} {...character} />
